26a194a47dc29ab84af9d6963ee7bdbfd04965f9
FlyingJester <foolkingcrown@gmail.com>
2014-07-15 11:29:26 -0700

    Fixed typo

--- literaltype.h
+++ literaltype.h
@@ -1,17 +1,6 @@
 #ifndef TS_LITERAL_TYPE_HEAD
 #define TS_LITERAL_TYPE_HEAD
 
-//Why can't we all even agree on THIS?
+		#define DOUBLE_LITERAL(x) x
+		#define FLOAT_LITERAL(x) x##f
 
-	#ifdef _WIN32
-		#define DOUBLE_LITERAL(x) x
-		#define FLOAT_LITERAL(x) x##f
-	#else
-		#define DOUBLE_LITERAL(x) x
-		#define FLOAT_LITERAL(x) x##f
-	#else
-		#define DOUBLE_LITERAL(x) x
-		#define FLOAT_LITERAL(x) x##f
-	#endif
-
-#endif
